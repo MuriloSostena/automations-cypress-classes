645b021655d6053564d6dbdc1cf0f64c4a074232
Murilo Sostena <murilosostena@gmail.com>
2025-10-03 12:08:35 -0300

    Page Objects altered

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -10,39 +10,14 @@ const menuPage = new MenuPage()
 const myInfoPage = new MyInfoPage()
 
 describe('Orange HRM Tests', () => {
-    const selectorsList = {
-        
-        firstNameField: '[name="firstName"]',
-        middleNameField: "[name='middleName']",
-        lastNameField: "[name='lastName']",
-        generecField: ".oxd-input--active",
-        dateField: "[placeholder='yyyy-dd-mm']",
-        closeDateField: '.--close',
-        submitButton: "[type='submit']",
-        dropDown: '.oxd-select-text--arrow'
-    } 
-
     it.only('User Info Update - Success', () => {
         loginPage.acessLoginPage()
         loginPage.loginWithAnyUser(userData.userSuccess.username, userData.userSuccess.password)
         dashboardPage.checkDashboardPage()
         menuPage.accessMyInfo()
-        myInfoPage.inputMyInfo()
-
-        // cy.get(selectorsList.firstNameField).clear().type("João")
-        // cy.get(selectorsList.middleNameField).clear().type("Da")
-        // cy.get(selectorsList.lastNameField).clear().type("Silva")
-        // cy.get(selectorsList.generecField).eq(3).clear().type("Joãozinho")
-        // cy.get(selectorsList.generecField).eq(4).clear().type("123456789")
-        // cy.get(selectorsList.generecField).eq(5).clear().type("22 11 33 55")
-        // cy.get(selectorsList.dateField).eq(0).clear().type("1989-01-01")
-        // cy.get(selectorsList.closeDateField).click({force: true})
-        // cy.get(selectorsList.dropDown).eq(0).click({force: true})
-        // cy.get(':nth-child(27) > span').click({force: true})
-        // cy.get(selectorsList.dropDown).eq(1).click({force: true})
-        // cy.get(':nth-child(3) > span').click({force: true})
-        // cy.get(selectorsList.submitButton).eq(0).click({force: true})       
-        // cy.get('.oxd-toast-close')
+        myInfoPage.fillPersonalDetails('First Name', 'Middle Name', 'Last Name')
+        myInfoPage.fillEmployeeId('EmployeeId', 'OtherId', 'DVNumber', '2025-01-01')
+        myInfoPage.fillNationality()
+        myInfoPage.saveForm()
     })
-
 })
